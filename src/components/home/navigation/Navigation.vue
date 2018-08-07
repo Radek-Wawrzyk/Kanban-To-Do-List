82c55a67e0300f2ba82e290416588f92d4582017
Radek <radek511@op.pl>
2018-08-07 22:14:05 +0200

    Edit - Navigation styles

--- src/components/home/navigation/Navigation.vue
+++ src/components/home/navigation/Navigation.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>navigation</h1>
+  <nav class="navigation">
+    <h1 class="navigation-logo">
+      <span>Kanban</span> ToDoList
+    </h1>
+    <div  class="navigation-panel">
+      <p class="navigation-panel-user">Fan Node.js</p>
+      <button class="navigation-panel-btn">
+        <span class="btn-plus">+</span>
+        <span class="btn-content">new Task</span>
+      </button>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -10,3 +21,4 @@ export default {
 
 </script>
 
+<style lang="scss" src="./navigation.scss"/>
