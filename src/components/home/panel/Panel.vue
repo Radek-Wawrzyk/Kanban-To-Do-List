cee61e6fbb98be8dc1fc8b7228651c178ac06397
Unknown <krolikplay@gmail.com>
2018-08-07 19:54:14 +0200

    Add - ToDoList  with basic styles (not responsive)

--- src/components/home/panel/Panel.vue
+++ src/components/home/panel/Panel.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="hello">
-  
+  <script
+
+
+
+  <div id="toDoBox">
+    <div id="toDoList-input">
+      <h1>
+        ToDoList
+      </h1>
+      <div class="inputGroup">
+        <input type="text" class="input-todo" placeholder="    New task">
+        <button class="button">
+          <i class="fas fa-plus"></i>
+        </button>
+      </div>
+    </div>
+    <div id="ToDoList-items">
+      <ul class="ToDoList">
+        <li class="ToDoListItem"><p>Uratować psa</p>  <i class="fas fa-check"></i></li>
+        <li class="ToDoListItem"><p>Zjeść bananka</p>  <i class="fas fa-check"></i></li>
+        <li class="ToDoListItem"><p>Przeprogramować deski</p>  <i class="fas fa-check"></i></li>
+      </ul>
+    </div>
   </div>
 </template>
 
